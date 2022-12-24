6454f2dd347b241e7f7e348149f4dc180edadebb
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2022-12-24 02:40:29 +0200

    Done

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+import Environment from './environment'
+import dotenv from 'dotenv'
+import Database from './database'
+import BaseURL from './base'
+
+//@desc dotenv enable configuration
+dotenv.config()
+
+class Config {
+  static PORT = process.env.PORT || 3000
+  static ENVIRONMENT = Environment.Development
+  static DATABASE = Database.Development
+  static BASEURL = BaseURL.Development
+}
+
+export default Config
